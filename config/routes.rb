04a98690b3c09a12df2c79fadd2f7c67d077b396
Mikegit1988 <mikenerly@gmail.com>
2022-01-02 20:54:17 -0500

    generate welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #here defining a route other than the default one..
+  root 'welcom#index'
 end
