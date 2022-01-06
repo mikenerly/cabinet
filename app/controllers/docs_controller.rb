46f32918b935dbcf474efe49f1ac9d1a352d6d78
Mikegit1988 <mikenerly@gmail.com>
2022-01-05 22:57:03 -0500

    Add Docs and creating views

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,15 +1,28 @@
 class DocsController < ApplicationController
     #Create the action for Docs controller
+    #Add a before action
+    before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
     def index
     end
 
     def show
+        
     end
 
     def new
+        #call the instance of the doc
+        @doc = Doc.new
     end
 
     def create
+        @doc = Doc.new(doc_params)
+
+        if @doc.save
+            redirect_to @doc
+        else
+            render 'new'
+        end
     end
 
     def edit
@@ -23,9 +36,11 @@ class DocsController < ApplicationController
 
     private
     def find_doc
+        @doc= Doc.find(params[:id])
     end
 
     def doc_params
+        params.require(:doc).permit(:title, :content)
     end
     
     
